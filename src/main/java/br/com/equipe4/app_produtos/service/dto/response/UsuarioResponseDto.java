a8a748c78a89957fb3182dd183638fff1dda69aa
marcelojacint <marcelojacintoferreirq@gmial.com>
2025-11-25 13:57:56 -0300

    feat: testes para usuário

--- src/main/java/br/com/equipe4/app_produtos/service/dto/response/UsuarioResponseDto.java
+++ src/main/java/br/com/equipe4/app_produtos/service/dto/response/UsuarioResponseDto.java
@@ -1,4 +1,4 @@
 package br.com.equipe4.app_produtos.service.dto.response;
 
-public record UsuarioResponseDto(Long id, String nome, String email, String senha) {
+public record UsuarioResponseDto(Long id, String nome, String email) {
 }
